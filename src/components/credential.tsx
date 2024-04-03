0eb191136750d6e8adf3e33ac6a4b306deb890f0
Eduardo Kleber <edukleber100@gmail.com>
2024-04-03 11:45:51 -0300

    Projeto finalizado

--- src/components/credential.tsx
+++ src/components/credential.tsx
@@ -1,19 +1,52 @@
-import { View, Text, Image, ImageBackground, TouchableOpacity,  } from "react-native";
+import { View, Text, Image, ImageBackground, TouchableOpacity, useWindowDimensions } from "react-native";
 
 import { Feather } from "@expo/vector-icons"
 
+import { MotiView } from "moti"
+
 import { colors } from "@/styles/colors";
+
 import { QRCode } from "@/components/qrcode";
 
+import { BadgeStore } from "@/store/badge-store";
+
 type Props = {
+    data: BadgeStore
     image?: string
     onChangeAvatar?: () => void
     onShowQRCode?: () => void
 }
 
-export function Credential({ onChangeAvatar,onShowQRCode, image }: Props){
+export function Credential({ data, onChangeAvatar, onShowQRCode, }: Props){
+
+    const {height} = useWindowDimensions()
+
     return (
-        <View className="w-full self-stretch items-center">
+        <MotiView
+         className="w-full self-stretch items-center"
+         from={{ 
+            opacity: 0,
+            translateY: -height,
+            rotateZ: "50deg",
+            rotateY: "30deg", 
+            rotateX: "30deg", 
+        }}
+         animate={{ 
+            opacity: 1, 
+            translateY: 0,
+            rotateZ: "0deg",
+            rotateY: "0deg", 
+            rotateX: "0deg", 
+        }}
+        transition={{
+            type: "spring",
+            damping: 20,
+            rotateZ: {
+                damping: 15,
+                mass: 3,
+            }
+        }}
+         >
             <Image 
             source={require("@/assets/ticket/band.png")} 
             className="w-24 h-52 z-10"
@@ -27,20 +60,20 @@ export function Credential({ onChangeAvatar,onShowQRCode, image }: Props){
                 >
 
                     <View className="w-full flex-row items-center justify-between">
-                        <Text className="text-zinc-50 text-sm font-bold">Unite summit</Text>
-                        <Text className="text-zinc-50 text-sm font-bold">#123</Text>
+                        <Text className="text-zinc-50 text-sm font-bold">{data.eventTitle}</Text>
+                        <Text className="text-zinc-50 text-sm font-bold">#{data.id}</Text>
                     </View>
 
                     <View className="w-40 h-40 bg-black rounded-full" />
                     
                 </ImageBackground>
 
-                {image ? (
+                {data.image ? (
                     <TouchableOpacity
                     onPressOut={onChangeAvatar}
                     >
                         <Image 
-                        source={{uri: image}} 
+                        source={{uri: data.image}} 
                         className="w-36 h-36 rounded-full -mt-24"
                         />
                     </TouchableOpacity>
@@ -54,14 +87,14 @@ export function Credential({ onChangeAvatar,onShowQRCode, image }: Props){
                 )}
 
                 <Text className="font-bold text-2xl text-zinc-50 mt-4">
-                    Eduardo Kleber
+                    {data.name}
                 </Text>
 
                 <Text className="font-regular text-base text-zinc-300 mb-4">
-                    edukleber100@gmail.com
+                    {data.email}
                 </Text>
 
-                <QRCode value="Eduardo Kleber" size={120}></QRCode>
+                <QRCode value={data.checkInURL} size={120}></QRCode>
 
                 <TouchableOpacity 
                 activeOpacity={0.7}
@@ -72,6 +105,6 @@ export function Credential({ onChangeAvatar,onShowQRCode, image }: Props){
                     </Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </MotiView>
     )
 }
